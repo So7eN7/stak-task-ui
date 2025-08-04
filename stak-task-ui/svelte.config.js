d521ff8e9e35d8b979a181d31d665dd029431179
so7en7 <soorenaaryan7@protonmail.com>
2025-08-04 16:17:13 +0330

    fall back update

--- stak-task-ui/svelte.config.js
+++ stak-task-ui/svelte.config.js
@@ -3,10 +3,11 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
+      fallback: 'index.html', 
       strict: false
     }),
     prerender: {
-      entries: [] 
+      entries: []
     }
   }
 };
